c9276b6f6ddccb0caf04925312eef655ee6edba5
m.sollie@joonsolutions.com <m.sollie@joonsolutions.com>
2021-09-30 08:39:23 +0000

    impact orderid fix

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -3,13 +3,13 @@
      select
         id as payment_id,
         orderid as order_id,
-        paymentmethod as paymentmethod,
+        paymentmethod as payment_method,
         status,
 
         -- amount is stored in cents, convert it to dollars
-        amount / 100 as amount,
+        {{ cents_to_dollars('amount') }} as amount,
         created as created_at
-    from  {{ source('stripe','payment')}}
+    from  `dbt-tutorial.stripe.payment`
  )
  
  select * from payments
